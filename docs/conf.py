6edb146057110dc775a4629a5a5d7d4fd8b44ef5
Michael Martorella <michaelmartorella@gmail.com>
2017-07-06 07:42:09 -0400

    Update sphinx docs - rebase

--- docs/conf.py
+++ docs/conf.py
@@ -31,11 +31,12 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'sphinx.ext.autosummary',]
+    'sphinx.ext.autosummary',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
