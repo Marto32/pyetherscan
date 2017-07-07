22cbb1f79119f730de1088aa43b4f8d33b3e47d8
Michael Martorella <michaelmartorella@gmail.com>
2017-07-06 22:06:59 -0400

    Add docs to response module

--- pyetherscan/response.py
+++ pyetherscan/response.py
@@ -1,13 +1,30 @@
+"""
+A module used to define API-specific response objects. All Etherscan API requests return an instance of :py:class:`EtherscanResponse`, extended to meet the endpoint's specific needs.
+
+See :doc:`/response` for an overview.
+"""
 import json
 
 from . import error
 
 
 class EtherscanResponse(object):
+    """
+    This is the parent class for all Etherscan API responses.
+
+    All child classes must define :py:meth:`parse_response`
+
+    Upon initialization, the class sets the following attributes:
+      - `etherscan_response`: The response json from Etherscan.
+      - `response_object`: The `requests.Response <http://docs.python-requests.org/en/master/api/#requests.Response>`_ returned by the API call.
+      - `status`: The Etherscan response status (independent of the `requests.Response` status).
+      - `message`: The Etherscan response message.
+      - Class-specific attributes are then set via the call to :py:meth:`parse_response`.
+
+    If a `403` error is received it will raise an :py:class:`EtherscanRequestError`. This typically means the rate limit has been reached. By default, :py:meth:`get_request` and :py:meth:`post_request` will handle this and automatically retry the request.
+    """
 
     def __init__(self, resp):
-        """
-        """
         if resp.status_code == 403:
             raise error.EtherscanRequestError(
                 'Rate limit reached.'
@@ -26,9 +43,9 @@ class EtherscanResponse(object):
             self.response_status_code = self.response_object.status_code
             self.status = self.etherscan_response.get('status')
             self.message = self.etherscan_response.get('message')
-            self._parse_response()
+            self.parse_response()
 
-    def _parse_response(self):
+    def parse_response(self):
         """
         The method that will parse the response object and store
         all attributes within the specific API response object.
@@ -36,6 +53,9 @@ class EtherscanResponse(object):
         raise NotImplementedError
 
     def __repr__(self):
+        """
+        Build a response representation like: `EtherscanResponse(resp=<Response 200>)`
+        """
         return '{_class}(resp={resp})'.format(
             _class=self.__class__.__name__,
             resp=self.response_object
@@ -43,8 +63,29 @@ class EtherscanResponse(object):
 
 
 class SingleAddressBalanceResponse(EtherscanResponse):
+    """
+    Represents a response object for a single address account balance call within the Etherscan `Accounts` endpoint.
 
-    def _parse_response(self):
+    Available attributes:
+      - `balance`: The balance of the address returned as a float.
+
+    Example:
+
+        .. code-block:: python
+            In [1]: response = SingleAddressBalanceResponse(resp)
+
+            In [2]: response.etherscan_response
+            Out[2]: {
+                "status":"1",
+                "message":"OK",
+                "result":"40807168564070000000000"
+            }
+
+            In [3]: response.balance
+            Out[3]: 40807168564070000000000.0
+    """
+
+    def parse_response(self):
         """
         Parses a single balance request response. Example API
         response output:
@@ -58,12 +99,48 @@ class SingleAddressBalanceResponse(EtherscanResponse):
                 }
 
         """
-        self.balance = int(self.etherscan_response.get('result'))
+        self.balance = float(self.etherscan_response.get('result'))
 
 
 class MultiAddressBalanceResponse(EtherscanResponse):
+    """
+    Represents a response object for a multi address account balance call within the Etherscan `Accounts` endpoint.
 
-    def _parse_response(self):
+    Available attributes:
+      - `balances`: The balances of the addresses returned as a dict.
+
+    Example:
+
+        .. code-block:: python
+            In [1]: response = MultiAddressBalanceResponse(resp)
+
+            In [2]: response.etherscan_response
+            Out[2]: {
+                "status":"1",
+                "message":"OK",
+                "result":[
+                    {
+                        "account":"0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a",
+                        "balance":"40807168564070000000000"
+                    }, {
+                        "account":"0x63a9975ba31b0b9626b34300f7f627147df1f526",
+                        "balance":"332567136222827062478"
+                    }, {
+                        "account":"0x198ef1ec325a96cc354c7266a038be8b5c558f67",
+                        "balance":"12005264493462223951724"
+                    }
+                ]
+            }
+
+            In [3]: response.balances
+            Out[3]: {
+                '0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a': 40807168564070000000000.0,
+                '0x63a9975ba31b0b9626b34300f7f627147df1f526': 332567136222827062478.0,
+                '0x198ef1ec325a96cc354c7266a038be8b5c558f67': 12005264493462223951724.0,
+            }
+    """
+
+    def parse_response(self):
         """
         Parses a multi balance request response. Example API
         response output:
@@ -98,7 +175,7 @@ class MultiAddressBalanceResponse(EtherscanResponse):
 
 class TransactionsByAddressResponse(EtherscanResponse):
 
-    def _parse_response(self):
+    def parse_response(self):
         """
         Parses a transactions by address request response. Example API
         response output:
@@ -139,7 +216,7 @@ class TransactionsByAddressResponse(EtherscanResponse):
 
 class TransactionsByHashResponse(EtherscanResponse):
 
-    def _parse_response(self):
+    def parse_response(self):
         """
         Parses a transactions by hash request response. Example API
         response output:
@@ -173,7 +250,7 @@ class TransactionsByHashResponse(EtherscanResponse):
 
 class BlocksMinedByAddressResponse(EtherscanResponse):
 
-    def _parse_response(self):
+    def parse_response(self):
         """
         Parses a blocks mined by address request response. Example API
         response output:
@@ -200,7 +277,7 @@ class BlocksMinedByAddressResponse(EtherscanResponse):
 
 class ContractABIByAddressResponse(EtherscanResponse):
 
-    def _parse_response(self):
+    def parse_response(self):
         """
         Parses a contract abi by address request response. Example API
         response output:
@@ -247,7 +324,7 @@ class ContractABIByAddressResponse(EtherscanResponse):
 
 class ContractStatusResponse(EtherscanResponse):
 
-    def _parse_response(self):
+    def parse_response(self):
         """
         Parses a transaction status by hash request response. Example API
         response output:
