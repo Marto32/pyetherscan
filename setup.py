db46482690e4b67e7c82f337a3936015fbb012f7
Michael Martorella <michaelmartorella@gmail.com>
2017-07-16 16:03:59 -0400

    Fix syntax error

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'requests',
         'retrying',
-    ]
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
