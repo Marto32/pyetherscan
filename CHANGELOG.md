fb9669f731bf58c196d128ebc893dfbb0ab18aa9
Michael Martorella <michaelmartorella@gmail.com>
2017-07-20 18:59:57 -0400

    Fix ConfigParser errors for python 2 users

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [0.1.2] - 2017-07-20
+### Changed
+- Fixed the ConfigParser error for python 2+.
+
 ## [0.1.1] - 2017-07-18
 ### Changed
 - Fixed python version errors in the `settings.py` file.
@@ -39,7 +43,8 @@ to @veox).
 - Added path identification support for both python 2 and 3 in the `setup.py`
   file to identify the `long_descripton` variable (previously failed for py2).
 
-[Unreleased]: https://github.com/Marto32/pyetherscan/compare/0.1.1...HEAD
+[Unreleased]: https://github.com/Marto32/pyetherscan/compare/0.1.2...HEAD
+[0.1.2]: https://github.com/Marto32/pyetherscan/compare/0.1.1...0.1.2
 [0.1.1]: https://github.com/Marto32/pyetherscan/compare/0.1.0...0.1.1
 [0.1.0]: https://github.com/Marto32/pyetherscan/compare/0.0.2...0.1.0
 [0.0.2]: https://github.com/Marto32/pyetherscan/compare/0.0.1...0.0.2
--- pyetherscan/settings.py
+++ pyetherscan/settings.py
@@ -1,19 +1,32 @@
 import os
+import sys
 
 HOME_DIR = os.path.expanduser('~')
 CONFIG_FILE = '.pyetherscan.ini'
 PATH = os.path.join(HOME_DIR, CONFIG_FILE)
 TESTING_API_KEY = 'YourApiKeyToken'
 
+
+def parse_configs(python_version, config_object):
+    """
+    A helper function to parse configuration files in
+    python 2 and 3.
+    """
+    if python_version < 3:
+        return config_object.get('Credentials', 'ETHERSCAN_API_KEY')
+    else:
+        return config_object['Credentials']['ETHERSCAN_API_KEY']
+
+
 if os.path.isfile(PATH):
     try:
         from configparser import ConfigParser
     except ImportError:
         # Handle python 2.7 code
-        import ConfigParser
+        from ConfigParser import ConfigParser
     config = ConfigParser()
     config.read(PATH)
-    ETHERSCAN_API_KEY = config['Credentials']['ETHERSCAN_API_KEY']
+    ETHERSCAN_API_KEY = parse_configs(sys.version_info[0], config)
 
 else:
     ETHERSCAN_API_KEY = os.environ.get('ETHERSCAN_API_KEY', TESTING_API_KEY)
